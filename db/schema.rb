124d6ac8e655500cc0f7510fb7662e192cf991a8
savannah-yahsuanlin <savannahwang75@gmail.com>
2023-03-09 17:10:16 +0800

    association

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
- # This file is auto-generated from the current state of the database. Instead
+# This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_08_163903) do
+ActiveRecord::Schema.define(version: 2023_03_09_085923) do
 
   create_table "messages", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
