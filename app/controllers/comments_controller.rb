deb0a16b3ad61f685ed61b82da3542b05d9e2531
savannah-yahsuanlin <savannahwang75@gmail.com>
2023-03-11 21:58:01 +0800

    edit update destroy comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,20 +1,45 @@
 class CommentsController < ApplicationController
+	before_action :find_message, only: [:create, :edit, :update, :destroy]
+	before_action :find_comment, only: [:edit, :update, :destroy]
 
 	def create
-		@message = Message.find(params[:id])
 		@comment = @message.comments.create(comment_params)
-		@comment.user_id = current_user.user_id
+		@comment.user_id = current_user.id
 
 		if @comment.save
-			redirect_to @message
+			redirect_to message_path(@message)
 		else
 			render 'new'
 		end 
+	end
+
+	def edit
+	end
+
+	def update
+		if @comment.update(comment_params)
+			redirect_to message_path(@message)
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@comment.destroy
+		redirect_to message_path(@message)
 	end
 
 	private
 
 	def comment_params
 		params.require(:comment).permit(:context)
+	end
+
+	def find_message
+		@message = Message.find(params[:message_id])
+	end
+
+	def find_comment
+		@comment = @message.comments.find(params[:id])
 	end
 end
